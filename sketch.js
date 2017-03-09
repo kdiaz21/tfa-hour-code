3fd9a0e7f5ba5122b9116fb16f421f4fdbf954c0
junior <jalvarado@wlapcs.org>
2017-03-09 23:55:49 +0000

    New sketch

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup(){
+    createCanvas(600,400);
+    background(255,105,180);
+}
+function draw(){
+    fill(255,255,255);
+    noStroke();
+    ellipse(50,200,50,50);
+    ellipse (200,200,50,50);
+    ellipse (mouseX,mouseY,50,50);
+    ellipse (500,200,50,50);
+    
+    
+}
